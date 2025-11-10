da439c31a4ce6cbac765054b3efc4ab4347a635e
Icey067 <flameluffy69@gmail.com>
2025-11-10 20:36:08 +0530

    1 (easy)

---
+++ LEETCODE/Easy/Two Sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        prevMap = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num 
+            if complement in prevMap:
+                return [prevMap[complement], i]
+
+            prevMap[num] = i
+        
