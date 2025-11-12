f7e6d0692ef9a4c2a48f3449a3e7b4099d25e9e6
Icey067 <flameluffy69@gmail.com>
2025-11-12 22:57:15 +0530

    done

--- LEETCODE/Easy/83. Remove Duplicates from Sorted List.py
+++ LEETCODE/Easy/83. Remove Duplicates from Sorted List.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def deleteDuplicates(self, head):
-        
         current = head
 
         while current and current.next:
-            
+
             if current.val == current.next.val:
                 current.next = current.next.next
 
