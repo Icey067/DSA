221eba8387d2ed37e37618a39515cb3e6e022edc
Icey067 <flameluffy69@gmail.com>
2025-11-14 18:12:09 +0530

    done

---
+++ LEETCODE/Easy/35. Search Insert Position.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        
+        low = 0 
+        high = len(nums) - 1
+
+        while low<=high:
+            mid = (low + high) / 2
+            guess = nums[mid]
+
+            if guess == target:
+                return mid
+
+            elif guess > target:
+                high = mid - 1
+
+            else:
+                low = mid + 1
+            
+        return low 
+        
