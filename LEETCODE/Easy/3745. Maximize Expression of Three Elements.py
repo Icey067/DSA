0cf658f3ec272bb36482e63d91851b851f734bd2
Icey067 <flameluffy69@gmail.com>
2025-11-16 23:27:05 +0530

    done

--- LEETCODE/Easy/3745. Maximize Expression of Three Elements.py
+++ LEETCODE/Easy/3745. Maximize Expression of Three Elements.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def maximizeExpressionOfThree(self, nums):scsc
+    def maximizeExpressionOfThree(self, nums):
         nums.sort()
 
         sum = nums[-1] + nums[-2] - nums[0]
